6068d96e7c9edeb33ef706ade5bb69852799cdfa
devshaffan <devshaffan@gmail.com>
2021-11-21 18:50:49 +0500

    Updates

---
+++ precache-manifest.266932c286996267534eaac24ebae3ad.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = [
+  {
+    "revision": "8947bbd3dfa5dcf88692",
+    "url": "/urdu_Time/static/css/main.bd53e3cf.chunk.css"
+  },
+  {
+    "revision": "8947bbd3dfa5dcf88692",
+    "url": "/urdu_Time/static/js/main.6d4de734.chunk.js"
+  },
+  {
+    "revision": "fee73d3d56d0f18e478a",
+    "url": "/urdu_Time/static/js/runtime~main.278483d4.js"
+  },
+  {
+    "revision": "cd07c7e68dcd7a51fbdc",
+    "url": "/urdu_Time/static/js/2.258ce6ce.chunk.js"
+  },
+  {
+    "revision": "34ade942072a19447366",
+    "url": "/urdu_Time/static/js/3.d8ecc626.chunk.js"
+  },
+  {
+    "revision": "6fc126f248e4b520c28a6229ca641bc0",
+    "url": "/urdu_Time/static/media/mango.6fc126f2.png"
+  },
+  {
+    "revision": "91412d063f2fde22edf3a015fa63a786",
+    "url": "/urdu_Time/static/media/removeItem.91412d06.mp3"
+  },
+  {
+    "revision": "821cde131dca119a05d6f45ed5115aad",
+    "url": "/urdu_Time/static/media/grocery.821cde13.gif"
+  },
+  {
+    "revision": "424d18986561c7039221eb97a66aef6b",
+    "url": "/urdu_Time/static/media/tostartpressbutton.424d1898.mp3"
+  },
+  {
+    "revision": "8efff185f64e4259ae68a4074dbfb0e6",
+    "url": "/urdu_Time/static/media/321go.8efff185.mp3"
+  },
+  {
+    "revision": "09b0b7bb4e8aad40481825ec9d91d223",
+    "url": "/urdu_Time/static/media/rightAnswer.09b0b7bb.mp3"
+  },
+  {
+    "revision": "ddcbc59b2917e3dbeb737fca1e3ede4e",
+    "url": "/urdu_Time/static/media/wrongAnswer.ddcbc59b.mp3"
+  },
+  {
+    "revision": "40d8e09c1bc1e01df2f2d0350718c089",
+    "url": "/urdu_Time/static/media/_1.40d8e09c.mp3"
+  },
+  {
+    "revision": "65a6b97dc7801719b2d140c796276265",
+    "url": "/urdu_Time/static/media/_2.65a6b97d.mp3"
+  },
+  {
+    "revision": "6bd419110b7fb9b6b44fd2f477a35abc",
+    "url": "/urdu_Time/static/media/_3.6bd41911.mp3"
+  },
+  {
+    "revision": "ae5c58d75da67259d8f8a689eb4040cd",
+    "url": "/urdu_Time/static/media/_4.ae5c58d7.mp3"
+  },
+  {
+    "revision": "fa32eb88ca708bfdd1f506d0d6ad7163",
+    "url": "/urdu_Time/static/media/_5.fa32eb88.mp3"
+  },
+  {
+    "revision": "639f363f9c8589d7472737d7d86ca22b",
+    "url": "/urdu_Time/static/media/_6.639f363f.mp3"
+  },
+  {
+    "revision": "7fc0455545ca15ac611aabfd2aa9d5fc",
+    "url": "/urdu_Time/static/media/_7.7fc04555.mp3"
+  },
+  {
+    "revision": "9c9b9bcb08b586e22914148c5f69bcd6",
+    "url": "/urdu_Time/static/media/_8.9c9b9bcb.mp3"
+  },
+  {
+    "revision": "fc30493cbaf5493f1204486e9e9cbbf1",
+    "url": "/urdu_Time/static/media/_9.fc30493c.mp3"
+  },
+  {
+    "revision": "594c0b0ed7da5faa738c12be102b3018",
+    "url": "/urdu_Time/static/media/_10.594c0b0e.mp3"
+  },
+  {
+    "revision": "2fd9ac2208c833754484473decfc84a1",
+    "url": "/urdu_Time/static/media/drop.2fd9ac22.wav"
+  },
+  {
+    "revision": "a0cc197d7b1c5ac20f1a5b0a60cc45ac",
+    "url": "/urdu_Time/static/media/typing.a0cc197d.mp3"
+  },
+  {
+    "revision": "d625354e0fe3750134af83e0e4faefd4",
+    "url": "/urdu_Time/static/media/apple.d625354e.png"
+  },
+  {
+    "revision": "411c4e6a0a9e83696dc8f57a70f342c0",
+    "url": "/urdu_Time/static/media/banana.411c4e6a.png"
+  },
+  {
+    "revision": "091d77928921a2ccefc1456f02198651",
+    "url": "/urdu_Time/static/media/orange.091d7792.png"
+  },
+  {
+    "revision": "da95b1d736ed03b78a44d44ef15d813a",
+    "url": "/urdu_Time/static/media/pear.da95b1d7.png"
+  },
+  {
+    "revision": "73fe00995cd7adebd696e8d1715500dc",
+    "url": "/urdu_Time/static/media/pineapple.73fe0099.png"
+  },
+  {
+    "revision": "5690694919b062e5d201f2895ca093b7",
+    "url": "/urdu_Time/static/media/plum.56906949.png"
+  },
+  {
+    "revision": "7b19bf7bd224cdd9941921ee087b2bdd",
+    "url": "/urdu_Time/static/media/pomegranate.7b19bf7b.png"
+  },
+  {
+    "revision": "2684d075fc2c72e38524ffc4f7d5df41",
+    "url": "/urdu_Time/static/media/strawberry.2684d075.png"
+  },
+  {
+    "revision": "d1d59a88467a6b381df3eee8ce160f25",
+    "url": "/urdu_Time/static/media/watermelon.d1d59a88.png"
+  },
+  {
+    "revision": "18ba7a065467efa32ebef20844f445f9",
+    "url": "/urdu_Time/static/media/lemon.18ba7a06.png"
+  },
+  {
+    "revision": "7498fd0032f151294c930c93367bd0f9",
+    "url": "/urdu_Time/static/media/carrot.7498fd00.png"
+  },
+  {
+    "revision": "560c5f81cbe99a953264d06e0c7cd014",
+    "url": "/urdu_Time/static/media/cherry.560c5f81.png"
+  },
+  {
+    "revision": "baee8b37557cf9ba7e6c7016c19d3b06",
+    "url": "/urdu_Time/static/media/peach.baee8b37.png"
+  },
+  {
+    "revision": "8506c659726847019116c0159642b02c",
+    "url": "/urdu_Time/static/media/dragon_fruit.8506c659.png"
+  },
+  {
+    "revision": "eede19c0d5f0fccbd13a8117167352e7",
+    "url": "/urdu_Time/static/media/base.eede19c0.svg"
+  },
+  {
+    "revision": "8d72f3b473dc3580bed7dcbc708bc8ff",
+    "url": "/urdu_Time/static/media/minute.8d72f3b4.svg"
+  },
+  {
+    "revision": "dbc7f1cfd8e0b8ada27fa1a14fb4956a",
+    "url": "/urdu_Time/static/media/hour.dbc7f1cf.svg"
+  },
+  {
+    "revision": "11d47bf7d1825e034a934d6174a08a3f",
+    "url": "/urdu_Time/static/media/HB.11d47bf7.svg"
+  },
+  {
+    "revision": "e1dbc008fd696f7e288500314e501429",
+    "url": "/urdu_Time/static/media/MB.e1dbc008.svg"
+  },
+  {
+    "revision": "a0d6b13874a29393b0f8252f2adb5fa7",
+    "url": "/urdu_Time/static/media/grocerySound.a0d6b138.mp3"
+  },
+  {
+    "revision": "18c8ae84a367ae39c7dc07370d5e133e",
+    "url": "/urdu_Time/static/media/paperBag.18c8ae84.png"
+  },
+  {
+    "revision": "e036ffe49c3290b294f2edb25b336a5d",
+    "url": "/urdu_Time/static/media/grapes.e036ffe4.png"
+  },
+  {
+    "revision": "992960b1c6434a2695d26d2e8ea74227",
+    "url": "/urdu_Time/static/media/rubix.992960b1.png"
+  },
+  {
+    "revision": "f5f258693cd37ccdf82ff06568ee9082",
+    "url": "/urdu_Time/static/media/background.f5f25869.gif"
+  },
+  {
+    "revision": "b512cd285a5ca16e6e3ec0c535047a76",
+    "url": "/urdu_Time/static/media/background2.b512cd28.gif"
+  },
+  {
+    "revision": "7360a269fa89ec8922a9fb9354820523",
+    "url": "/urdu_Time/static/media/sound.7360a269.png"
+  },
+  {
+    "revision": "f02ac89a44b28acd80cb6b0399adbf8f",
+    "url": "/urdu_Time/static/media/mute.f02ac89a.png"
+  },
+  {
+    "revision": "605607b171e11597115e8f3a551c91e1",
+    "url": "/urdu_Time/index.html"
+  }
+];
